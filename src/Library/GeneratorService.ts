c326039de0d7afeec9af1de11c6b2bcc8449a987
Rawphs <raphaelaprotasio@gmail.com>
2018-10-25 14:38:18 +0200

    fix(GeneratorService): mkdir for generateModule and add crud option to generateController

--- src/Library/GeneratorService.ts
+++ src/Library/GeneratorService.ts
@@ -7,32 +7,42 @@ export class GeneratorService {
   @inject(Config)
   private config: Config;
 
-  public async generateController ({ name, field }: { name: string, field: string | string[] }) {
+  public async generateController (output: Output, { name, module, crud }: { name: string, module: string, crud: boolean }) {
     const { config, nameFormats } = this.prepare(name);
-    const { pascalCased } = nameFormats;
-    const fields = Array.isArray(field) ? field : [ field ];
+    const { pascalCased }         = nameFormats;
+    const moduleName              = this.formatNames(module).pascalCased;
+    const params                  = { ...nameFormats };
 
+    if (crud) {
+      Object.assign(params, {
+        imports  : [
+          `import { dbActions } from 'stix-wetland';`,
+          `import { ${pascalCased} } from '../Entity/${pascalCased}';`,
+        ].join('\n'),
+        dbActions: `@dbActions(${pascalCased})\n`,
+      });
+    }
 
+    const destination = path.resolve(
+      config.moduleRoot,
+      moduleName,
+      'src',
+      'Controller',
+      `${pascalCased}Controller.ts`,
+    );
 
-    const parameters = {
-      ...nameFormats,
-      imports: [
-        `import { dbActions } from 'stix-wetland';`,
-        `import { ${pascalCased} } from '../Entity/${pascalCased}';`,
-      ].join('\n'),
-      dbActions: '@dbActions(pascalCased)\n',
-    };
+    await File.generate(config.templates.controller, destination, params);
 
-    const destination = path.resolve(config.moduleRoot, pascalCased, 'src', `${pascalCased}Controller`);
-
-
-    await File.generate(config.templates.controller, destination, this.formatNames(name));
+    output.success(`${pascalCased}Controller created.`);
   }
 
-  public async generateModule ({ name }: { name: string }) {
+  public async generateModule (output: Output, { name }: { name: string }) {
     const { config, nameFormats } = this.prepare(name);
-    const modulePath = path.resolve(config.moduleRoot, nameFormats.pascalCased);
-    const importName = `${nameFormats.pascalCased}Module`;
+    const modulePath              = path.resolve(config.moduleRoot, nameFormats.pascalCased);
+    const importName              = `${nameFormats.pascalCased}Module`;
+
+    await File.mkdir(modulePath);
+
     const file = await File.readGlob('**/*', { cwd: config.templates.module });
 
     await file.replace(nameFormats).write(modulePath);
@@ -42,20 +52,22 @@ export class GeneratorService {
       .addImport(`../module/${nameFormats.pascalCased}`, importName)
       .modify(/( +)\w+,?\n];\s*$/, { custom: (match: string, space: string) => `${space}${importName},\n${match}` })
       .update();
+
+    output.success(`Created module ${nameFormats.pascalCased}`);
   }
 
   public formatNames (name: string): NameFormatsType {
-    const normalized = name[0].toLowerCase() + name.substr(1);
-    const camelCased = normalized.replace(/-([a-z])/ig, ( x, character ) => character.toUpperCase());
+    const normalized  = name[0].toLowerCase() + name.substr(1);
+    const camelCased  = normalized.replace(/-([a-z])/ig, (x, character) => character.toUpperCase());
     const pascalCased = camelCased[0].toUpperCase() + camelCased.substr(1);
-    const upperCased = camelCased.replace(/([A-Z])/g, c => `_${c}`).toUpperCase();
+    const upperCased  = camelCased.replace(/([A-Z])/g, c => `_${c}`).toUpperCase();
 
     return { camelCased, pascalCased, upperCased };
   }
 
   private prepare (name: string): { config: GeneratorConfigType, nameFormats: NameFormatsType } {
     return {
-      config: this.config.of<GeneratorConfigType>('generator'),
+      config     : this.config.of<GeneratorConfigType>('generator'),
       nameFormats: this.formatNames(name),
     };
   }
